293d9a59fd5e090c1bf8b21caa57aed45db23934
Sahadeb <np03a190065@heraldcollege.edu.np>
2022-05-25 07:43:31 +0545

    Pizza order program using dart

---
+++ IfElseStatement.dart
@@ -0,0 +1,9 @@
+//If Else Statement
+void main() {
+  var num = 11;
+  if (num % 2 == 0) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
---
+++ Pizzaordering.dart
@@ -0,0 +1,16 @@
+void main() {
+  const pizzaPrices = {
+    'margherita': 5.5,
+    'pepperoni': 7.5,
+    'vegetarian': 6.5,
+  };
+  const order = ['margherita', 'pepperoni', 'pineapple'];
+  var total = 0.0;
+  for (var item in order) {
+    final price = pizzaPrices[item];
+    if (price != null) {
+      total += price;
+    }
+  }
+  print('Total: \$$total');
+}
